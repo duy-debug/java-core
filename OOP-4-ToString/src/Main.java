df88a950d42c5b4a6dd97ac2e917431cdaca3635
Tran Mai Ngoc Duy <tmnduy.it@gmail.com>
2025-01-28 14:13:55 +0700

    First commit

---
+++ OOP-4-ToString/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        MyDate m1 = new MyDate(15,5,2025);
+        MyDate m2 = new MyDate(11,1,2022);
+        MyDate m3 = new MyDate(11,1,2021);
+        System.out.println(m1); // 15/5/2025
+        System.out.println(m2);
+        System.out.println(m3);
+    }
+}
