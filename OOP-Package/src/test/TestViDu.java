bee141b3b53c1e84aba371b05fccd50fbb89388a
Tran Mai Ngoc Duy <tmnduy.it@gmail.com>
2025-02-04 08:44:49 +0700

    Update

---
+++ OOP-Package/src/test/TestViDu.java
@@ -0,0 +1,12 @@
+package test;
+
+import code.ViDu;
+public class TestViDu {
+  public static void main(String[] args) {
+    ViDu v = new ViDu(5,6);
+    System.out.println(v.getA());
+    code_b.ViDu vd = new code_b.ViDu(1,2,3);
+    System.out.println(vd.getC());
+
+  }
+}
